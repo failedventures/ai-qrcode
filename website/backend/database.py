799bfb1544821bb45341d4fb15dd45243ae0700f
Yannis Zarkadas <yanniszark@gmail.com>
2024-01-29 22:34:46 -0500

    small fix
    Signed-off-by: Yannis Zarkadas <yanniszark@gmail.com>

--- website/backend/database.py
+++ website/backend/database.py
@@ -2,8 +2,7 @@ from uuid import uuid4
 
 from sqlalchemy import create_engine
 from contextlib import contextmanager
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, String, Integer, Boolean
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
